703c588aa3db5bf69625895627ff1180867a6515
Vishal41 <pal.vishal41@gmail.com>
2017-09-09 23:12:20 +0530

    release 0.0.5

--- droidclub/models.py
+++ droidclub/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Info(models.Model):
+	author = models.CharField(max_length=255)
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+
+	def __str__(self):
+		return self.author + self.title
+
+class Team(models.Model):
+	name = models.CharField(max_length=255)
+	image = models.ImageField(upload_to='clubs/droidclub/team')
+	designation = models.CharField(max_length=255)
+	facebook = models.URLField(null=True)
+	github = models.URLField(null=True)
+	twitter =  models.URLField(null=True)
+
+	def __str__(self):
+		return self.name + self.designation
