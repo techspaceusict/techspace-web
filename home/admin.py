fdd33b4b3036e1bc3d63df71d465380a9f09c106
Vishal41 <pal.vishal41@gmail.com>
2017-09-10 15:17:24 +0530

    release 0.0.9

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Contact, Club, Association 
 # Register your models here.
-admin.site.register(Contact, list_display=['name', 'email','content'])
+admin.site.register(Contact, list_display=['name', 'email','content','app_name'])
 admin.site.register(Club)
 admin.site.register(Association)
