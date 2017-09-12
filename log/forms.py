a391161f50008ae26e07da788389f74c474ed62b
Prafful Kumar <kumarprafful.54@gmail.com>
2017-09-12 22:20:23 +0530

    0.2.1

--- log/forms.py
+++ log/forms.py
@@ -26,6 +26,10 @@ class BlogAddForm(forms.ModelForm):
 	class Meta:
 		model = BlogPost
 		fields = ['title','image' , 'content']
+		widgets = { 'title': forms.TextInput(attrs={'placeholder': 'Title', 'class': 'form-control'}),
+					'content': forms.Textarea(attrs={'placeholder': 'Write your post...', 'class': 'form-control' }),
+
+		}
 
 
 class InfoAddForm(forms.ModelForm):
@@ -33,6 +37,10 @@ class InfoAddForm(forms.ModelForm):
 	class Meta:
 		model = Info
 		fields = ['title', 'content']
+		widgets = { 'title': forms.TextInput(attrs={'placeholder': 'type "information"'}),
+				'content': forms.Textarea(attrs={'placeholder': 'Describe your club...'}),
+
+		}
 
 
 class TeamAddForm(forms.ModelForm):
@@ -40,6 +48,16 @@ class TeamAddForm(forms.ModelForm):
 	class Meta:
 		model = Team
 		fields = ['name', 'image', 'designation', 'facebook', 'github', 'linkedin']
+		widgets = { 'name': forms.TextInput(attrs={'placeholder': 'Name'}),
+					'designation': forms.TextInput(attrs={'placeholder': 'Designation'}),
+					'facebook': forms.TextInput(attrs={'placeholder': 'facebook'}),
+					'github': forms.TextInput(attrs={'placeholder': 'github'}),
+					'linkedin': forms.TextInput(attrs={'placeholder': 'linkedin'}),
+
+
+
+
+		}
 
 
 class EventAddForm(forms.ModelForm):
@@ -47,6 +65,12 @@ class EventAddForm(forms.ModelForm):
 	class Meta:
 		model = Events
 		fields = ['title', 'date', 'venue', 'content']
+		widgets = { 'title': forms.TextInput(attrs={'placeholder':'Title', 'class': 'form-title'}),
+				'venue': forms.TextInput(attrs={'placeholder': 'Venue', 'class': 'form-control'}),
+				'content': forms.Textarea(attrs={'placeholder': 'Enter the details about the event...'})
+
+
+		}
 
 
 
