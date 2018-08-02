5ae11206ade79db8e0247c9cfbd53a79c0202cd5
dheer1206 <dheergulati12@gmail.com>
2018-08-02 19:16:10 +0530

    latest temp

--- latest/models.py
+++ latest/models.py
@@ -7,8 +7,45 @@ from datetime import datetime
 class Latestpost(models.Model) :
 
     title = models.CharField(max_length=255)
-    datetime = models.DateTimeField(default=datetime.now)
-    content = models.TextField()
+    author = models.CharField(max_length=255)
+	date = models.DateField()
+	time = models.TimeField()
+	image = models.ImageField(upload_to='events', default='blog/thumbnail-default.jpg')
+	venue = models.CharField(max_length=1024)
+	content = models.TextField()
+	slug = models.SlugField(db_index=True, unique=True, max_length=2024)
 
-    def __str__(self):
-    	return self.title
+	codeschool = 'codeschool'
+	cogitans = 'cogitans'
+	droidclub = 'droidclub'
+	ecell = 'ecell'
+	electrotech = 'electrotech'
+	oslc = 'oslc'
+	renderedusict = 'renderedusict'
+	turingai = 'turingai'
+
+	club_choices = (
+			(codeschool, 'Codeschool'),
+			(cogitans, 'Cogitans'),
+			(droidclub, 'Droid Club'),
+			(ecell, 'E-Cell'),
+			(electrotech, 'Electrotech'),
+			(oslc, 'OSLC'),
+			(renderedusict, 'Rendered-USICT'),
+			(turingai, 'Turing A.I.'),
+
+		)
+
+	club = models.CharField(max_length=200, choices=club_choices, blank=True)
+
+	def get_absolute_url(self):
+		return reverse('event:detail', args=[self.id, self.slug])
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.slug = slugify(self.title)
+		super(Events, self).save(*args, **kwargs)
+
+
+	def __str__(self):
+		return self.title + ' - ' + self.club
--- latest/views.py
+++ latest/views.py
@@ -7,4 +7,15 @@ from .models import Latestpost
 class LatestPostsList(ListView) :
     model = Latestpost
     context_object_name = 'latest-posts'
-    template_name = 'event/index.html'
+    template_name = 'latest/index.html'
+
+class LatestPostsDetail(DetailView) :
+    model = Latestpost
+    context_object_name = 'latest-posts-detail'
+    template_name = 'latest/index.html'    
+
+
+class EventDetailView(DetailView):
+	model = Events
+	context_object_name = 'event_detail'
+	template_name = 'event/event_detail.html'
