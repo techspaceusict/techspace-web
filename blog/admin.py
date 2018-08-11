c23cbb1de0777452e769fead44477ac284466ace
Prafful kumar <kumarprafful.54@gmail.com>
2018-08-11 15:04:12 +0530

    prepopulated slug field

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,8 @@ from .models import BlogPost, Comments, Tag, Upvote
 
 # Register your models here.
 
-admin.site.register(BlogPost)
+admin.site.register(BlogPost,prepopulated_fields = {"slug": ("title",),})
 admin.site.register(Comments, list_display=['comment_author', 'comment_text', 'comment_date'])
 
 admin.site.register(Tag)
 admin.site.register(Upvote)
-
