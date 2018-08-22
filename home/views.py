7ecfd52e0ae2f2f8ca39c7c0ecc0bba17532b11d
Prabhanshu <guptaprabhanshu1@gmail.com>
2018-08-23 01:42:22 +0530

    Remove one time fix_email method.

--- home/views.py
+++ home/views.py
@@ -8,21 +8,12 @@ from log.models import UserProfile
 from django.contrib.auth.models import User
 # Create your views here.
 
-def fix_email():
-	users = User.objects.all()
-	for user in users:
-		profile = UserProfile.objects.get(user=user)
-		if not profile.email:
-			profile.email = user.email
-			profile.save()
-
 def index(request):
 	blogs = BlogPost.objects.order_by('-date')[:5]
 	#clubs
 	club_objects = Club.objects.all()
 	app_urls = ['codeschool:index','cogitans:index','droidclub:index','ecell:index','electrotech:index','oslc:index','renderedusict:index','turingai:index']
 	clubs = zip(club_objects,app_urls)
-	fix_email()
 	#associations
 	# assocs = Association.objects.all()
 	return render(request, 'home/index.html', {'clubs':clubs, 'blogs':blogs})
