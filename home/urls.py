db63cca48f18f094884cc12488023a1a2ac653b1
vishal41 <pal.vishal41@gmail.com>
2018-08-23 23:48:27 +0530

    added urlize tag in blog/post detail and achievements view added

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
-from .views import index#, contactSubmit
+from .views import index, achievements#, contactSubmit
 
 app_name = 'home'
 
 urlpatterns = [
 	url(r'^$', index, name='index'),
+	url(r'^achievements/$',achievements, name='achievements')
 	# url(r'^contact/submit/$', contactSubmit, name='contactSubmit'),
 
 ]
