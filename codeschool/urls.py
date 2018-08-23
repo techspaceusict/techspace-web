d33a979413c3dd7f884093cbf7d25ac68d5824b8
akshatpapnoi <akshatpapnoi1110@gmail.com>
2018-08-24 03:41:43 +0530

    confirmation on delete and codeschool template setup

--- codeschool/urls.py
+++ codeschool/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from .views import index, BlogPostView, EventListView
+from django.views.generic import TemplateView
 
 app_name = 'codeschool'
 
@@ -7,4 +8,9 @@ urlpatterns = [
 	url(r'^$',index,name='index'),
 	url(r'^blogs/$', BlogPostView.as_view(), name='post'),
 	url(r'^events/$', EventListView.as_view(), name='events'),
+	url(r'^team/$', TemplateView.as_view(template_name='clubs/codeschool/team.html'), name='teams'),
+	url(r'^classes/$', TemplateView.as_view(template_name='clubs/codeschool/classes.html'), name='classes'),
+	url(r'^contests/$', TemplateView.as_view(template_name='clubs/codeschool/contests.html'), name='contests'),
+	url(r'^calendar/$', TemplateView.as_view(template_name='clubs/codeschool/calendar.html'), name='calendar'),
+	url(r'^connect/$', TemplateView.as_view(template_name='clubs/codeschool/connect.html'), name='connect'),
 ]
--- codeschool/views.py
+++ codeschool/views.py
@@ -12,7 +12,7 @@ def index(request):
 	# info = Info.objects.filter(club=Info.codeschool).order_by('id').last()
 	# team = Team.objects.filter(club=Team.codeschool)
 	event = Events.objects.filter(club=Events.codeschool).order_by('-date').first()
-	return render(request,'clubs/codeschool/index.html', {'event': event})
+	return render(request,'clubs/codeschool/index2.html', {'event': event})
 
 class BlogPostView(BlogListView):
 
