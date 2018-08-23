ba14498b8ace8dcdede6942f9c40b875251d4bc1
Prafful kumar <kumarprafful.54@gmail.com>
2018-08-23 22:58:01 +0530

    userprofile admin display

--- log/admin.py
+++ log/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import UserProfile, Report, Message, Notification
 
 # Register your models here.
-admin.site.register(UserProfile, list_display=['user', 'email', 'course', 'institution', 'year_of_graduation', 'user_badge_text'])
+admin.site.register(UserProfile, list_display=['user','name' , 'email', 'course', 'institution', 'year_of_graduation', 'user_badge_text'])
 admin.site.register(Report)
 admin.site.register(Message)
 admin.site.register(Notification)
