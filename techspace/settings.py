10e1e69f666b63c43265352408e08ba1f4a068e6
Prafful kumar <kumarprafful.54@gmail.com>
2018-08-25 13:55:10 +0530

    settings bug

--- techspace/settings.py
+++ techspace/settings.py
@@ -107,23 +107,14 @@ WSGI_APPLICATION = 'techspace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'techspace',
-        'USER': 'root',
-        'PASSWORD': 'root1234'
-    }
 }
 
-SILENCED_SYSTEM_CHECKS = ['mysql.E001']
+# SILENCED_SYSTEM_CHECKS = ['mysql.E001']
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
