e9a38e348d66fff4c4ad10570d189cc204afe012
Prafful Kumar <kumarprafful.54@gmail.com>
2018-09-24 13:34:46 +0530

    Hall of fame URL repaired

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	url(r'^$', index, name='index'),
 	url(r'^achievements/$',achievements, name='achievements'),
 	url(r'^responsible_disclosure$', responsible_disclosure, name='responsible_disclosure' ),
-	url(r'^hall_of_fame/%hall_of_fame$', hall_of_fame, name='hall_of_fame'),
+	url(r'^hall_of_fame/$', hall_of_fame, name='hall_of_fame'),
 
 
 	# url(r'^contact/submit/$', contactSubmit, name='contactSubmit'),
